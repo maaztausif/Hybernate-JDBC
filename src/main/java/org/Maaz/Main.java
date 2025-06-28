c999f12021db45c2491f43048c69fe4dcd25db80
maaztausif <maaztausif48@gmail.com>
2025-06-29 01:51:32 +0200

    insert data and create table

--- src/main/java/org/Maaz/Main.java
+++ src/main/java/org/Maaz/Main.java
@@ -1,17 +1,37 @@
 package org.Maaz;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Student s1 = new Student();
+        s1.setRollNo(3);
+        s1.setsName("Maaz");
+        s1.setsAge(25);
+
+        Configuration cfg = new Configuration();
+
+        cfg.addAnnotatedClass(org.Maaz.Student.class);
+        cfg.configure();
+
+        SessionFactory sF = cfg.buildSessionFactory();
+        Session session = sF.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        session.persist(s1);
+
+
+        transaction.commit();
+
+        System.out.println(s1);
+
     }
 }
